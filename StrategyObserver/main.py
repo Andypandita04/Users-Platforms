8602542ee8c3b2ce4352eec73c76db3a0d2bc6b9
Leonardo Espinosa <100044786+leointhecode@users.noreply.github.com>
2024-02-19 22:54:27 -0600

    Add write_on_file

--- StrategyObserver/main.py
+++ StrategyObserver/main.py
@@ -1,6 +1,15 @@
 from User import User
 #from Plataform import Platform, StrategySpotifyFree, StrategyAmazon, StrategyDisney, StrategyHBO, StrategyNetflix, StrategySpotify
 from Plataform import Platform, StrategySpotifyFree,  StrategySpotifyPremium
+
+def write_on_file(texto, nombre_archivo):
+    # Asegura que el nombre del archivo tenga la extensión .txt
+    if not nombre_archivo.endswith(".txt"):
+        nombre_archivo += ".txt"
+    
+    # Abre el archivo en modo de añadir o crea uno nuevo si no existe, luego escribe el texto en una nueva línea
+    with open(nombre_archivo, "a") as archivo:
+        archivo.write(texto + "\n")
  
 spotify = Platform("Spotify", StrategySpotifyPremium())
 
@@ -41,9 +50,3 @@ print("\nMES 5\n\n")
 spotify.execute()
 
 
-
-
-
-#TODO Revisar notify
-#TODO Cambiar sistema de pago para tomar en cuenta la cuenta de los meses
-#TODO Implementar response en .txt
